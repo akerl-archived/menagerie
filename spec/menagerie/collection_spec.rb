04edbb900b205d4bfb587b7f7d04f4264b1724dc
Les Aker <me@lesaker.org>
2015-02-14 10:00:58 -0500

    adjust spec examples and add orphan spec

--- spec/menagerie/collection_spec.rb
+++ spec/menagerie/collection_spec.rb
@@ -8,6 +8,7 @@ describe Menagerie::Collection do
     it 'parses releases in a collection' do
       Dir.chdir("#{examples}/existing") do
         expect(collection.releases.size).to eql 3
+        expect(collection.releases.first).to be_a Menagerie::Release
       end
     end
 
@@ -23,6 +24,21 @@ describe Menagerie::Collection do
     it 'supports an alternate path for releases' do
       x = Menagerie.new paths: { releases: 'spec/examples/existing/releases' }
       expect(x.releases.size).to eql 3
+    end
+  end
+
+  describe '#orphans' do
+    it 'identifies orphaned artifacts' do
+      Dir.chdir("#{examples}/orphaned") do
+        expect(collection.orphans.size).to eql 1
+        expect(collection.orphans.first).to be_a String
+      end
+    end
+
+    it 'returns an empty list if there are no orphans' do
+      Dir.chdir("#{examples}/existing") do
+        expect(collection.orphans.size).to eql 0
+      end
     end
   end
 end
