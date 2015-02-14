ed71c5e4933a599cf344aa53d572c42736be755b
Les Aker <me@lesaker.org>
2015-02-14 09:31:49 -0500

    add tests for releases

--- spec/menagerie/collection_spec.rb
+++ spec/menagerie/collection_spec.rb
@@ -1,1 +1,28 @@
 require 'spec_helper'
+
+describe Menagerie::Collection do
+  let(:examples) { 'spec/examples' }
+  let(:collection) { Menagerie.new }
+
+  describe '#releases' do
+    it 'parses releases in a collection' do
+      Dir.chdir("#{examples}/existing") do
+        expect(collection.releases.size).to eql 3
+      end
+    end
+
+    it 'parses an empty collection as empty' do
+      Dir.chdir("#{examples}/empty") do
+        expect(collection.releases.size).to eql 0
+      end
+      Dir.chdir("#{examples}/empty_with_dirs") do
+        expect(collection.releases.size).to eql 0
+      end
+    end
+
+    it 'supports an alternate path for releases' do
+      x = Menagerie.new paths: { releases: 'spec/examples/existing/releases' }
+      expect(x.releases.size).to eql 3
+    end
+  end
+end
